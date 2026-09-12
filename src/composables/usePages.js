b1ffb0d41c8f952a289dc9483d509ba2b4fb33ed
nkpiksalia <natalkakielbicka@gmail.com>
2026-09-12 20:15:38 +0200

    Share pages state across components

--- src/composables/usePages.js
+++ src/composables/usePages.js
@@ -1,15 +1,13 @@
 import { ref } from 'vue'
 import { initialPages } from '../data/pages'
 
+const pages = ref([...initialPages])
+
+const getPageById = (pageId) => {
+  return pages.value.find((page) => page.id === pageId)
+}
+
 export const usePages = () => {
-  const pages = ref([...initialPages])
-
-  const getPageById = (pageId) => {
-    return pages.value.find((page) => {
-      return page.id === pageId
-    })
-  }
-
   return {
     pages,
     getPageById,

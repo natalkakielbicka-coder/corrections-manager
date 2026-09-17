cf595246ed64180d48fe16732b4a8833727a479e
nkpiksalia <natalkakielbicka@gmail.com>
2026-09-17 13:34:49 +0200

    Initialize WordPress plugin

---
+++ corrections-manager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Corrections Manager
+ * Description: Panel do zgłaszania i obsługi poprawek na stronie internetowej.
+ * Version: 1.0.0
+ * Author: Natalia Kiełbicka
+ * Text Domain: corrections-manager
+ */
+
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+define('CORRECTIONS_MANAGER_VERSION', '1.0.0');
+define('CORRECTIONS_MANAGER_FILE', __FILE__);
+define('CORRECTIONS_MANAGER_PATH', plugin_dir_path(__FILE__));
+define('CORRECTIONS_MANAGER_URL', plugin_dir_url(__FILE__));
